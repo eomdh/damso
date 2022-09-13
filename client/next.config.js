e9a14e4e316bd65d4082049e6584a1641c51fd59
eomdh <edh9692@gmail.com>
2022-09-13 22:25:43 +0900

    webpack.config.js 삭제

--- client/next.config.js
+++ client/next.config.js
@@ -1,23 +1,3 @@
 const withImages = require('next-images');
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
 
-module.exports = withImages({
-  webpack(config, options) {
-    return config
-  }
-});
-
-module.exports = {
-  compress: true,
-  webpack(config, { webpack }) {
-    const prod = process.env.NODE_ENV === 'production';
-    return {
-      ...config,
-      mode: prod ? 'production' : 'development',
-      devtool: prod ? 'hidden-source-map' : 'eval',
-      plugins: [...config.plugins],
-    };
-  },
-};  
+module.exports = withImages();

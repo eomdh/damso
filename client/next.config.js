66781dd5e41e5c9d57f73db7674c59d037296f1e
eomdh <edh9692@gmail.com>
2022-09-13 23:17:42 +0900

    analyzer

--- client/next.config.js
+++ client/next.config.js
@@ -1,3 +1,21 @@
+const withPlugins = require('next-compose-plugins');
 const withImages = require('next-images');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
-module.exports = withImages();
+module.exports = withPlugins([withBundleAnalyzer, withImages], {
+  compress: true,
+  webpack(config, { webpack }) {
+    const prod = process.env.NODE_ENV === 'production';
+    return {
+      ...config,
+      mode: prod ? 'production' : 'development',
+      devtool: prod ? 'hidden-source-map' : 'eval',
+      plugins: [
+        ...config.plugins,
+        new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/ko$/),
+      ],
+    };
+  },
+});
